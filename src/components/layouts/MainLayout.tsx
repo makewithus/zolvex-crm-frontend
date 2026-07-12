95ccdc12920e109670dac9160ac3238ba056d424
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-12 19:35:14 +0530

    Refactor global MainLayout container alignment

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -25,12 +25,14 @@ export const MainLayout = () => {
 
   return (
     <SidebarContext.Provider value={{ isCollapsed, toggleSidebar }}>
-      <div className="flex min-h-screen flex-col bg-background">
+      <div className="flex h-full flex-col bg-background overflow-hidden">
         <Header />
-        <div className="flex flex-1 overflow-hidden">
+        <div className="flex flex-1 overflow-hidden min-h-0">
           <Sidebar />
-          <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 relative">
-            <Outlet />
+          <main className="flex-1 overflow-y-auto relative" id="main-content">
+            <div className="p-4 md:p-6 lg:p-8">
+              <Outlet />
+            </div>
           </main>
         </div>
       </div>
