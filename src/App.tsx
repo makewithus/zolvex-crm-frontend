50a3b42927c6966def96c8c39dc30be31e680c67
Akshit Bhandula <akshitbhandula@gmail.com>
2026-07-16 16:52:11 +0530

    Refactor foundational App component entry configuration mappings

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,17 @@ function App() {
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-      <Toaster position="top-right" richColors />
+      <Toaster 
+        position="top-right" 
+        toastOptions={{
+          classNames: {
+            toast: 'font-sans text-[13px] rounded border border-slate-200 shadow-none bg-white text-slate-900',
+            success: '!bg-slate-950 !text-white !border-slate-900',
+            error: '!bg-red-50 !text-red-700 !border-red-100',
+            info: '!bg-slate-50 !text-slate-700 !border-slate-100',
+          }
+        }}
+      />
     </QueryClientProvider>
   );
 }
